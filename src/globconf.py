855136d2b1bb6f305cc308baa6f11f0369159536
Thomas <76688099+ThMountainMan@users.noreply.github.com>
2022-01-10 17:40:32 +0100

    Update globconf.py

--- src/globconf.py
+++ src/globconf.py
@@ -69,14 +69,14 @@ class Yaml(object):
             return yaml.dump(data, fp)
 
 
-#class Python(object):
-#    """Load Python file and give back the data as dict."""
+class Python(object):
+    """Load Python file and give back the data as dict."""
 
-def read(self, path):
-    locals_ = {}
-    exec(open("./filename").read(), {}, locals_)
-    # execfile(path, {}, locals_)
-    return locals_
+    def read(self, path):
+        locals_ = {}
+        exec(open("./filename").read(), {}, locals_)
+        # execfile(path, {}, locals_)
+        return locals_
 
 class Ini(object):
     """Load Ini file and give back the data as dict."""
